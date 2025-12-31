bca1f3122631f463fe7b78a4707148f29c2baa79
rajanchoksi <git@github.com>
2025-12-31 23:45:08 +0530

    add some comments in main function

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from display import display_result
 from colors import YELLOW, RESET
 
 def main():
+    # getting and storing user input
     user_input: str = input("\n- Enter an interger number here: ")
 
+    # asking user what kind of data format user want to see in result
     data_format: str = input (f"- Enter data format flag ({YELLOW}b{RESET} for binary, {YELLOW}h{RESET} for hex, {YELLOW}o{RESET} for octal and {YELLOW}a{RESET} for all): ")
 
+    # storing type casted integer for further use
     input_number: int = casting_str_to_number(user_input)
 
+    # displaying result based on user's data format choise
     display_result(data_format, input_number)
 
 
